2cf120ddf13a138b281cd1a09e90ea9ef7e0f704
zizhengdong0412 <zizheng.dong0412@gmail.com>
2025-02-25 20:56:30 -0500

    Changed the way to deploy the video

--- dog.js
+++ dog.js
@@ -1,13 +1,13 @@
 const dogImage = document.getElementById("dog-image");
-    const fetchDogButton = document.getElementById("fetch-dog");
-    function fetchRandomDog() {
+const fetchDogButton = document.getElementById("fetch-dog");
+function fetchRandomDog() {
     fetch("https://dog.ceo/api/breeds/image/random")
         .then(response => response.json())
         .then(data => {
         dogImage.src = data.message;
     })
 }
-    fetchRandomDog();
+fetchRandomDog();
 
-    // Click it. New Dog
-    fetchDogButton.addEventListener("click", fetchRandomDog);
+// Click it. New Dog
+fetchDogButton.addEventListener("click", fetchRandomDog);
